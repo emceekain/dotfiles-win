4d67b05d0bcd07ca0d79dae42c8ad0abc1ccb44f
Brent McKain <emceekain@gmail.com>
2019-04-03 12:35:15 -0500

    Added DoubleCommander shortcuts file to version control.

--- DotFilesPush.ps1
+++ DotFilesPush.ps1
@@ -2,4 +2,5 @@ $SOURCE=$env:appdata
 $DEST="H:\.dotfiles"
 
 Copy-Item $SOURCE\doublecmd\doublecmd.xml -Destination $DEST\doublecmd\
+Copy-Item $SOURCE\doublecmd\shortcuts.scf -Destination $DEST\doublecmd\
 # WinMerge: Manually import/export the .ini file.
